ae18f6778512277c3204833c8ebe191a2874defe
Otep <63043217+Otep123@users.noreply.github.com>
2025-11-23 13:31:17 -0500

    Auto-generated commit

--- src/app/shared/directives/slide-content/slide-content.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SlideContentDirective } from './slide-content.directive';
-
-describe('SlideContentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SlideContentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/shared/directives/viewport-observer/viewport-observer.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ViewportObserverDirective } from './viewport-observer.directive';
-
-describe('ViewportObserverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ViewportObserverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
